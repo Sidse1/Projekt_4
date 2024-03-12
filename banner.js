b950712ad61837deaa5d991eaeae30943036fb75
leneholstt <lene7315@icloud.com>
2024-03-12 15:50:17 +0100

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,14 +1,23 @@
 let slideIndex = 0;
-showSlides();
+  showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3500); // Skift hvert 3. sekund (3000 millisekunder)
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); // Change image every 2 seconds
-}
+
+  // Next/previous controls
+  function plusSlides(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("mySlides");
+    if (slideIndex > slides.length) {slideIndex = 1}
+    else if (slideIndex < 1) {slideIndex = slides.length}
+    showSlides();
+  }
