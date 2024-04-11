59de5cdfb6457774d24c395aaca58fe2451a7d3d
Sidse1 <sidsebj1912@gmail.com>
2024-04-11 11:49:45 +0200

    Update nyhedsbrev.js

--- nyhedsbrev.js
+++ nyhedsbrev.js
@@ -2,7 +2,7 @@ document.getElementById("newsletterform").addEventListener("submit", function(ev
   event.preventDefault(); // Prevent form submission
   
   // Get the email input value
-  var email = document.getElementById("email").value;
+  let email = document.getElementById("email").value;
   
   // Display pop-up message
   alert("Tak fordi du tilmeldte dig nyhedsbrevet med følgende e-mail: " + email);
