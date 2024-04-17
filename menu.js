ed5e004da215f6f6e3bb00e5fc086b970046d3a7
leneholstt <lene7315@icloud.com>
2024-04-17 11:54:32 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,66 +1,43 @@
 
 
-let sidebarWidth = 250; 
-
+let sidebarWidth = 250;
 
 function openNav() {
   document.getElementById("mySidebar").style.width = sidebarWidth + "px";
   document.getElementById("main2").style.marginRight = sidebarWidth + "px";
 }
 
-
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main2").style.marginRight = "0";
 }
 
-
-let sidebarOpen = false; 
+let sidebarOpen = false;
 
 function toggleNav() {
   if (sidebarOpen) {
-    closeNav(); 
+    closeNav();
     sidebarOpen = false;
   } else {
-    openNav(); 
+    openNav();
     sidebarOpen = true;
   }
 }
 
-
-for (let i = 0; i < 5; i++) {
-  
-  toggleNav();
-}
-
-
-let buttons = document.querySelectorAll('.sidebar-button'); 
-
+let buttons = document.querySelectorAll('.sidebar-button');
 
 buttons.forEach(button => {
   button.addEventListener('click', toggleNav);
+  toggleNav(); 
 });
 
-
 function updateSidebarWidth(newWidth) {
-  let sidebarWidth = newWidth; 
+  sidebarWidth = newWidth; 
   document.getElementById("mySidebar").style.width = sidebarWidth + "px";
   document.getElementById("main2").style.marginRight = sidebarWidth + "px";
 }
 
-
-let a = 5;
-let b = '5';
-
-if (a === b) {
-  console.log('Variablerne er ens');
-} else {
-  console.log('Variablerne er ikke ens');
-}
-
-
 let message = 'Velkommen til vores hjemmeside!';
 console.log(message);
-
 
 document.getElementById('welcome-message').textContent = message;
